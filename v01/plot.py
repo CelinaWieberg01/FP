ea44505ce877e8cd89a2f78b47aa8e5e3c54a406
Aaron Schink <aaron.schink@tu-dortmund.de>
2025-01-27 14:25:45 +0100

    nkjn

--- v01/plot.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt         # Für Plots
-import numpy as np                      # Fürs Rechnen
-from uncertainties import ufloat        # zahl = ufloat(nominal_value, std_devs)
-import uncertainties.unumpy as unp      # wie ufloat nur mit arrays
-from scipy.optimize import curve_fit    # params, cov = curve_fit(fitfunktion, x-wert, y-wert, cov=True)
-import scipy.constants as constants         # z.B. h = constants.h für planckzahl
-
