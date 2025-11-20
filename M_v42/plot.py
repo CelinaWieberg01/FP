b2f3d8b3c40ae37ec9774a72293e3a10098f6d5e
Aaron Schink <aaron.schink@tu-dortmund.de>
2025-11-20 16:29:57 +0100

    auswertung HOPG

--- M_v42/plot.py
+++ M_v42/plot.py
@@ -6,42 +6,93 @@ from scipy.optimize import curve_fit    # params, cov = curve_fit(fitfunktion, x
 import scipy.constants as constants         # z.B. h = constants.h für planckzahl
 import numpy.fft as fft
 from skimage.feature import peak_local_max
+from skimage.filters import gaussian
+
+def sort_cyclic(points): # funktion um die peaks zyklisch zu sortieren
+
+    pts = np.asarray(points) # makes sure dass wir ein array haben
+
+    cx, cy = pts.mean(axis=0) # berechnet centroid
+
+    angles = np.arctan2(pts[:,1] - cy, pts[:,0] - cx) # berechnet winkel 
+
+    order = np.argsort(angles) # sortiert nach winkel
+
+    return pts[order], order
 
 
-# --- load (your values) ---
-data_1 = np.genfromtxt("data/1.csv", delimiter=";")
-pxs = data_1.shape[0]         # image size (pixels; assume square)
-scale = 2.31                  # total scan size in nm (IMPORTANT: total scan, not nm/pixel)
-fac = scale / pxs    # nm per real-space pixel
+px_imagesize = 128  # px
+nm_imagesize = 2.31 # nm
+scaling = nm_imagesize/px_imagesize # nm/px
 
-# --- show real-space image (optional) ---
-plt.imshow(data_1, origin="lower")
-plt.title("Real-space")
-plt.colorbar()
-plt.show()
+show = False # parameter die bstrimmen ob wir bilder zeigen und oder speichern
+save = False
 
-# --- FFT pipeline (as you had it) ---
-f_data_1 = fft.ifftshift(data_1)
-f_data_1 = fft.fft2(f_data_1)
-f_data_1 = fft.fftshift(f_data_1)
-f_data_1 = np.abs(f_data_1)
+def lattice_constant(number):
+    data = gaussian(np.genfromtxt(f"data/{number}.csv", delimiter=";")) 
 
-# --- detect peaks in FFT magnitude ---
-peaks = peak_local_max(f_data_1, min_distance=5, threshold_rel=0.50, num_peaks=8)
-print("Detected peaks (row, col):\n", peaks)
+    plt.imshow(data, origin="lower", cmap="BuPu", extent=[0,nm_imagesize, 0,nm_imagesize]) # plot data
+    plt.colorbar()
+    plt.tight_layout()
 
-# --- plot FFT and peaks to visually inspect ---
-plt.imshow(np.log1p(f_data_1), origin="lower")   # log-scale makes weaker peaks visible
-plt.scatter(peaks[:,1], peaks[:,0], c="red", s=20)
-plt.title("FFT (log scale) with detected peaks")
-plt.show()
+    if show == True:
+        plt.show()
+    if save == True:
+        plt.savefig(f"plots/{number}_meas.pdf")
 
-d01 = np.sqrt((peaks[0,0]/fac-peaks[1,0]/fac)**2 + (peaks[0,1]/fac-peaks[1,1]/fac)**2)
-d02 = np.sqrt((peaks[0,0]/fac-peaks[2,0]/fac)**2 + (peaks[0,1]/fac-peaks[2,1]/fac)**2)
-d03 = np.sqrt((peaks[0,0]/fac-peaks[3,0]/fac)**2 + (peaks[0,1]/fac-peaks[3,1]/fac)**2)
+    plt.figure()
 
-d12 = np.sqrt((peaks[1,0]/fac-peaks[2,0]/fac)**2 + (peaks[1,1]/fac-peaks[2,1]/fac)**2)
-d23 = np.sqrt((peaks[3,0]/fac-peaks[2,0]/fac)**2 + (peaks[3,1]/fac-peaks[2,1]/fac)**2)
+    f = fft.ifftshift(data) # fouriertransformation in diesem abschnitt
+    f = fft.fft2(f)
+    f = fft.fftshift(f)
+    f = np.abs(f)**2 # und power spectrum darstellen
 
-print(1/d01, 1/d02, 1/d03, 1/d12, 1/d23)
+    freqs = np.fft.fftfreq(px_imagesize, d=scaling) # so skalieren dass wir im richtigen reziproken raum (1/nm) sind
+    freqs = np.fft.fftshift(freqs)
+    kx = freqs
+    ky = freqs
+
+    peaks = peak_local_max(f, min_distance=5, num_peaks=6) # peaks finden und sortieren
+    sorted_peaks, order = sort_cyclic(peaks)
+
+    kx_axis = np.fft.fftshift(np.fft.fftfreq(px_imagesize, d=scaling))
+    ky_axis = kx_axis.copy()
+
+    kx_peaks = kx_axis[sorted_peaks[:,1]]
+    ky_peaks = ky_axis[sorted_peaks[:,0]]
+    
+
+    plt.imshow(f, origin="lower", extent=[kx[0], kx[-1], ky[0], ky[-1]])
+    plt.tight_layout()
+    plt.colorbar()
+
+    col = ["red", "blue", "green", "white", "orange", "purple"] # ignorieren
+    #for i in range(6):
+    #    plt.scatter(kx_peaks[i], ky_peaks[i], facecolors="none", edgecolors=col[i], s=100, label=f"{i}")
+    #plt.legend()
+
+    if show == True:
+        plt.show()
+    if save == True:
+        plt.savefig(f"plots/{number}_ft.pdf")
+
+    k_distances = np.sqrt((kx_peaks - np.roll(kx_peaks, -1))**2 + (ky_peaks - np.roll(ky_peaks, -1))**2) # berechne abstände zwischen den peaks
+    k_distances = ufloat(np.mean(k_distances), np.std(k_distances)/np.sqrt(6)) 
+    r_distances = 2/(np.sqrt(3)*k_distances) # berechne länge des gittervektors vom reziproken gittervektor
+    print(f"Distances {number} = {r_distances} nm")
+
+    plt.figure()
+
+    return r_distances
+
+save = True
+r1 = lattice_constant(1)
+lattice_constant(0)
+r2 = lattice_constant(2)
+r3 = lattice_constant(3)
+r4 = lattice_constant(4)
+r = np.array((unp.nominal_values(r1), unp.nominal_values(r2), unp.nominal_values(r3), unp.nominal_values(r4)))
+r_avg = ufloat(np.mean(unp.nominal_values(r)), np.std(unp.nominal_values(r))/np.sqrt(4))
+print("Average distance = ", r_avg, " nm")
+print("Deviation from literature = ", 100*np.abs(r_avg - 0.246)/0.246, " %")
 
