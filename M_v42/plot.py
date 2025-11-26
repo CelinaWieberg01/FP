c418b5cc5f4047b4287c9061ea6572ed3ae9ea1f
Aaron Schink <aaron.schink@tu-dortmund.de>
2025-11-26 01:20:36 +0100

    mezhr auswertung, plots verbessert

--- M_v42/plot.py
+++ M_v42/plot.py
@@ -25,23 +25,18 @@ px_imagesize = 128  # px
 nm_imagesize = 2.31 # nm
 scaling = nm_imagesize/px_imagesize # nm/px
 
-show = False # parameter die bstrimmen ob wir bilder zeigen und oder speichern
 save = False
 
 def lattice_constant(number):
-    data = gaussian(np.genfromtxt(f"data/{number}.csv", delimiter=";")) 
+    data = gaussian(np.genfromtxt(f"data/{number}.csv", delimiter=";"))
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(12,5), constrained_layout=True, dpi=600)
 
-    plt.imshow(data, origin="lower", cmap="BuPu", extent=[0,nm_imagesize, 0,nm_imagesize]) # plot data
-    plt.xlabel(r"$x$ in \si{\nano\meter}")
-    plt.ylabel(r"$y$ in \si{\nano\meter}")
-    plt.colorbar(label=r"Intensität in arbiträren Einheiten")
+    im1 = ax1.imshow(data, origin="lower", cmap="BuPu",
+                     extent=[0, nm_imagesize, 0, nm_imagesize])
+    ax1.set_xlabel(r"$x$ in \si{\nano\meter}")
+    ax1.set_ylabel(r"$y$ in \si{\nano\meter}")
+    fig.colorbar(im1, ax=ax1, label=r"Intensität in arbiträren Einheiten")
 
-    if show == True:
-        plt.show()
-    if save == True:
-        plt.savefig(f"plots/{number}_meas.pdf")
-
-    plt.figure()
 
     f = fft.ifftshift(data) # fouriertransformation in diesem abschnitt
     f = fft.fft2(f)
@@ -62,34 +57,24 @@ def lattice_constant(number):
     kx_peaks = kx_axis[sorted_peaks[:,1]]
     ky_peaks = ky_axis[sorted_peaks[:,0]]
     
+    im2 = ax2.imshow(f, origin="lower", extent=[kx[0], kx[-1], ky[0], ky[-1]], interpolation='bilinear')
+    ax2.set_xlabel(r"$k_x$ in \si{\per\nano\meter}")
+    ax2.set_ylabel(r"$k_y$ in \si{\per\nano\meter}")
+    fig.colorbar(im2, ax=ax2, label=r"Intensität in arbiträren Einheiten")
 
-    plt.imshow(f, origin="lower", extent=[kx[0], kx[-1], ky[0], ky[-1]])
-    plt.xlabel(r"$k_x$ in \si{\per\nano\meter}")
-    plt.ylabel(r"$k_y$ in \si{\per\nano\meter}")
-    plt.colorbar(label=r"Intensität in arbiträren Einheiten")
-
-    col = ["red", "blue", "green", "white", "orange", "purple"] # ignorieren
-    #for i in range(6):
-    #    plt.scatter(kx_peaks[i], ky_peaks[i], facecolors="none", edgecolors=col[i], s=100, label=f"{i}")
-    #plt.legend()
-
-    if show == True:
-        plt.show()
-    if save == True:
-        plt.savefig(f"plots/{number}_ft.pdf")
+    if save:
+        fig.savefig(f"plots/both_plots_{number}.pdf")
 
     k_distances = np.sqrt((kx_peaks - np.roll(kx_peaks, -1))**2 + (ky_peaks - np.roll(ky_peaks, -1))**2) # berechne abstände zwischen den peaks
     k_distances = ufloat(np.mean(k_distances), np.std(k_distances)/np.sqrt(6)) 
     r_distances = 2/(np.sqrt(3)*k_distances) # berechne länge des gittervektors vom reziproken gittervektor
     print(f"Distances {number} = {r_distances} nm")
 
-    plt.figure()
-
     return r_distances
 
 save = True
 r1 = lattice_constant(1)
-lattice_constant(0)
+r0 =lattice_constant(0)
 r2 = lattice_constant(2)
 r3 = lattice_constant(3)
 r4 = lattice_constant(4)
@@ -99,9 +84,9 @@ print("Average distance = ", r_avg, " nm")
 print("Deviation from literature = ", 100*np.abs(r_avg - 0.246)/0.246, " %")
 
 
-
 heights = np.array((1.64, 1.63, 1.11, 1.76, 1.35, 1.58))
 
 height = ufloat(np.mean(heights), np.std(heights)/np.sqrt(len(heights)))
 
 print(f"Height = {height} nm")
+
